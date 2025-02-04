7166ae0e8ee184d20cee143cc6d0dc9d6f636fdf
rutheyd <r8507603@gmail.com>
2025-02-04 08:47:53 +0200

    :)

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,4 @@
 import { createBrowserRouter } from "react-router";
-// import UserPage from "./components/UserPage";
-// import NavBar from "./components/NavBar";
-// import Person from "./components/Person";
 import AppLayout from "./components/AppLayout";
 import RecipeList from "./components/recipes/RecipesList";
 import RecipeToShow from "./components/recipes/RecipeToShow";
